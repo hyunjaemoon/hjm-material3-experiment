16679ccefcb8b3f3951d25d9fe1a32cf6f290b0c
문현재 <hyunjaemoon@MacBook-Pro-2.attlocal.net>
2023-02-25 02:14:27 -0800

    Added top app bar and navigation elements.
    - Modified package.json to concurrently reflect ts changes

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,28 @@ import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/checkbox/checkbox.js';
 import '@material/web/slider/slider.js'
+import '@material/web/navigationbar/navigation-bar.js'
+import '@material/web/navigationdrawer/navigation-drawer.js'
+import '@material/web/navigationtab/navigation-tab.js'
+import './web-elements/hjm-top-app-bar'
 import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
+
 @customElement('hjm-web')
 export class HjmWeb extends LitElement {
+
+    // Instantiation
+
     render() {
         return html `
+            <hjm-top-app-bar></hjm-top-app-bar>
+            <md-navigation-bar data-aria-label="HJM Bar">
+                <md-navigation-drawer></md-navigation-drawer>
+                <md-navigation-tab>About</md-navigation-tab>
+                <md-navigation-tab>Projects</md-navigation-tab>
+                <md-navigation-tab>Links</md-navigation-tab>
+            </md-navigation-bar>
             <md-outlined-button label="Back"></md-outlined-button>
             <md-filled-button label="Next"></md-filled-button>
             <md-slider></md-slider>
